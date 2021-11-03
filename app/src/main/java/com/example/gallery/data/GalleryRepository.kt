26272b6e25848cd58de3947171b7be8d2d80aae6
Vitali Maroz <vitali.maroz@nbc-189-71b.icx.local>
2021-11-03 18:37:45 +0300

    try populate map with single cursor

--- app/src/main/java/com/example/gallery/data/GalleryRepository.kt
+++ app/src/main/java/com/example/gallery/data/GalleryRepository.kt
@@ -6,12 +6,12 @@ import com.example.gallery.data.entity.Folder
 
 import java.io.File
 
-import android.os.Environment
 import com.example.gallery.data.entity.Image
 import android.net.Uri
 import android.provider.MediaStore
 import androidx.core.content.FileProvider.getUriForFile
 import com.example.gallery.sdk29AndUp
+import java.util.ArrayList
 
 
 class GalleryRepository(private val context: Context) {
@@ -24,13 +24,72 @@ class GalleryRepository(private val context: Context) {
         // TODO: 10/25/21 return the fullscreen image
     }
 
+    private fun getAllFoldersWithImages(): MutableList<Folder> {
+
+        val collection = sdk29AndUp {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
+        } ?: MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+        val projection = arrayOf(
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.DISPLAY_NAME,
+            MediaStore.Images.Media.WIDTH,
+            MediaStore.Images.Media.HEIGHT,
+            MediaStore.Images.Media.DATA,
+            MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
+            MediaStore.Images.Media.BUCKET_ID,
+        )
+
+        val photos = mutableListOf<Image>()
+        val folders = mutableListOf<Folder>()
+        val imagesByUris = mutableMapOf<String, MutableList<Image>>()
+
+        return (context.contentResolver.query(
+            collection,
+            projection,
+            null,
+            null,
+            "${MediaStore.Images.Media.DISPLAY_NAME} ASC"
+        )?.use { cursor ->
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+            val displayNameColumn =
+                cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
+            val widthColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.WIDTH)
+            val heightColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.HEIGHT)
+            val bucketDisplayName = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME)
+
+            val uris = mutableSetOf<String>()
+
+            val isEmpty = !(cursor.moveToFirst()) || cursor.count == 0
+
+            if (isEmpty) println("Cursor is empty when loading images")
+
+            if (!isEmpty) {
+                do {
+                    val id = cursor.getLong(idColumn)
+                    val displayName = cursor.getString(displayNameColumn)
+                    val width = cursor.getInt(widthColumn)
+                    val height = cursor.getInt(heightColumn)
+                    val contentUri = ContentUris.withAppendedId(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        id
+                    )
+                    photos.add((Image(id, displayName, width, height, contentUri)))
+                    uris.add(cursor.getString(bucketDisplayName))
+                    imagesByUris.putIfAbsent()
+                } while (cursor.moveToNext())
+            }
+            uris.forEach { folders.add(Folder()) }
+        } ?: listOf()).toMutableList()
+    }
+
     private fun getFoldersWithImages(): MutableList<Folder> {
 
         val resultFolders = mutableListOf<Folder>()
 
-        val foldersWithImages = getImageFolderNames()
+        val foldersWithImagesNames = getImageFolderNames()
 
-        foldersWithImages.forEach { folderName ->
+        foldersWithImagesNames.forEach { folderName ->
             val images = mutableListOf<Image>()
             val photos = loadPhotosFromExternalStorage(folderName)
             images.addAll(photos)
@@ -107,18 +166,22 @@ class GalleryRepository(private val context: Context) {
             val widthColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.WIDTH)
             val heightColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.HEIGHT)
 
-            if (!(cursor.moveToFirst()) || cursor.count == 0) println("Cursor is empty when loading images")
+            val isEmpty = !(cursor.moveToFirst()) || cursor.count == 0
 
-            while (cursor.moveToNext()) {
-                val id = cursor.getLong(idColumn)
-                val displayName = cursor.getString(displayNameColumn)
-                val width = cursor.getInt(widthColumn)
-                val height = cursor.getInt(heightColumn)
-                val contentUri = ContentUris.withAppendedId(
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                    id
-                )
-                photos.add((Image(id, displayName, width, height, contentUri)))
+            if (isEmpty) println("Cursor is empty when loading images")
+
+            if (!isEmpty) {
+                do {
+                    val id = cursor.getLong(idColumn)
+                    val displayName = cursor.getString(displayNameColumn)
+                    val width = cursor.getInt(widthColumn)
+                    val height = cursor.getInt(heightColumn)
+                    val contentUri = ContentUris.withAppendedId(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        id
+                    )
+                    photos.add((Image(id, displayName, width, height, contentUri)))
+                } while (cursor.moveToNext())
             }
             photos.toList()
         } ?: listOf()
