daad70b559538797950fa875bf0c145ee0f936c0
vitali-itechart <vitali.maroz@itechart-group.com>
2022-01-06 17:35:07 +0300

    test fixed to work with flows

--- app/src/test/java/com/example/gallery/RepositoryTest.kt
+++ app/src/test/java/com/example/gallery/RepositoryTest.kt
@@ -4,10 +4,14 @@ import android.content.ContentResolver
 import android.content.Context
 import android.database.MatrixCursor
 import android.provider.MediaStore
+import androidx.compose.runtime.collectAsState
 import androidx.core.net.toUri
 import com.example.gallery.data.GalleryRepository
 import com.example.gallery.data.entity.Folder
 import com.example.gallery.data.entity.Image
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -61,11 +65,17 @@ class RepositoryTest {
         repo = GalleryRepository(ctx)
     }
 
+    @InternalCoroutinesApi
     @Test
     fun `get folders with images`() {
 
         val imageData = arrayOf(
-            "44", "download (1)", "191", "119", "Downloads", "content://media/external/images/media/44)"
+            "44",
+            "download (1)",
+            "191",
+            "119",
+            "Downloads",
+            "content://media/external/images/media/44)"
         )
 
         val imageData2 = arrayOf(
@@ -77,7 +87,12 @@ class RepositoryTest {
         )
 
         val imageData4 = arrayOf(
-            "48", "downloadasdasd", "192", "111", "Camera", "content://media/external/images/media/48)"
+            "48",
+            "downloadasdasd",
+            "192",
+            "111",
+            "Camera",
+            "content://media/external/images/media/48)"
         )
 
         val imageData5 = arrayOf(
@@ -87,15 +102,45 @@ class RepositoryTest {
         val result = listOf(
             Folder(
                 "Downloads", listOf(
-                    Image(44, "download (1)", 191, 119, "content://media/external/images/media/44".toUri()),
-                    Image(46, "download", 192, 111, "content://media/external/images/media/46".toUri()),
+                    Image(
+                        44,
+                        "download (1)",
+                        191,
+                        119,
+                        "content://media/external/images/media/44".toUri()
+                    ),
+                    Image(
+                        46,
+                        "download",
+                        192,
+                        111,
+                        "content://media/external/images/media/46".toUri()
+                    ),
                 )
             ),
             Folder(
                 "Camera", listOf(
-                    Image(47, "download123", 192, 111, "content://media/external/images/media/47".toUri()),
-                    Image(48, "downloadasdasd", 192, 111, "content://media/external/images/media/48".toUri()),
-                    Image(49, "downloadqqq", 192, 111, "content://media/external/images/media/49".toUri()),
+                    Image(
+                        47,
+                        "download123",
+                        192,
+                        111,
+                        "content://media/external/images/media/47".toUri()
+                    ),
+                    Image(
+                        48,
+                        "downloadasdasd",
+                        192,
+                        111,
+                        "content://media/external/images/media/48".toUri()
+                    ),
+                    Image(
+                        49,
+                        "downloadqqq",
+                        192,
+                        111,
+                        "content://media/external/images/media/49".toUri()
+                    ),
                 )
 
             )
@@ -106,10 +151,15 @@ class RepositoryTest {
         imagesCursor.addRow(imageData4)
         imagesCursor.addRow(imageData5)
 
-//        repo.getContent.collect()
-//        { throwable, list ->
-//            println(throwable)
-//            assert(result.containsAll(list))
-//        }
+
+
+        runBlocking {
+
+            repo.getContent().collect { content ->
+
+                assert(result.containsAll(content.folders))
+            }
+        }
+
     }
 }
